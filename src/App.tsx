365ff0e89e7492167d35af90d2ec0fbe1c2cc4ba
gayans <gayanzampaths@gmail.com>
2024-05-13 00:16:42 +0530

    PF:SD:Inital Implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import DashboardPage from './pages/DashboardPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<DashboardPage />} />
+          <Route path='/dashboard' element={<DashboardPage />} />
+        </Routes>
+      </BrowserRouter>
+    </React.Fragment>
   );
 }
 
---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const DashboardPage = () => {
+    return(
+        <React.Fragment>
+            <Navbar />
+        </React.Fragment>
+    )
+}
+
+export default DashboardPage;
