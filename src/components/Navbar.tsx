5e7147e9619b21eaa314260c89615b115d9b0c58
gayans <gayanzampaths@gmail.com>
2024-05-13 01:06:34 +0530

    PF:SD:Navbar implementation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,125 @@
-import React from "react";
+import React, { MouseEvent, useState } from "react";
+import MenuIcon from '@mui/icons-material/Menu';
+import { AppBar, Avatar, Box, Button, Container, IconButton, Menu, MenuItem, ThemeProvider, Toolbar, Tooltip, Typography, createTheme } from "@mui/material";
+
+import logo from '../assets/logo/pf-logo.png';
+import { useNavigate } from "react-router-dom";
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+})
 
 const Navbar = () => {
-    return(
-        <React.Fragment>
 
+    const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+    const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+
+    const navigate = useNavigate();
+
+    const handleOpenNavMenu = (event: MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event: MouseEvent<HTMLElement>) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = (path: string) => {
+        setAnchorElNav(null);
+        navigate(path);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+        <React.Fragment>
+            <ThemeProvider theme={darkTheme} >
+                <AppBar position="static">
+                    <Container maxWidth="xl">
+                        <Toolbar disableGutters>
+                            <Box component="img" sx={{ display: { xs: 'none', md: 'flex' }, height: 70, margin: 2 }} alt="Planner Fusion" src={logo} />
+
+                            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                                <IconButton
+                                    size="large"
+                                    aria-label="account of current user"
+                                    aria-controls="menu-appbar"
+                                    aria-haspopup="true"
+                                    onClick={handleOpenNavMenu}
+                                    color="inherit"
+                                >
+                                    <MenuIcon />
+                                </IconButton>
+                                <Menu
+                                    id="menu-appbar"
+                                    anchorEl={anchorElNav}
+                                    anchorOrigin={{
+                                        vertical: 'bottom',
+                                        horizontal: 'left',
+                                    }}
+                                    keepMounted
+                                    transformOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'left',
+                                    }}
+                                    open={Boolean(anchorElNav)}
+                                    onClose={handleCloseNavMenu}
+                                    sx={{
+                                        display: { xs: 'block', md: 'none' },
+                                    }}
+                                >
+                                    <MenuItem key='dashboard' onClick={() => handleCloseNavMenu('/dashboard')}>
+                                        <Typography textAlign="center"> Dashboard </Typography>
+                                    </MenuItem>
+                                </Menu>
+                            </Box>
+                            
+                            <Box component="img" sx={{ display: { xs: 'flex', md: 'none' }, height: 70, margin: 2 }} alt="Planner Fusion" src={logo} />
+                            
+                            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                                <Button
+                                    key='dashboard'
+                                    onClick={() => handleCloseNavMenu('/dashboard')}
+                                    sx={{ my: 2, color: 'white', display: 'block' }}
+                                >
+                                    Dashboard
+                                </Button>
+                            </Box>
+
+                            <Box sx={{ flexGrow: 0 }}>
+                                <Tooltip title="Open settings">
+                                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                        <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                                    </IconButton>
+                                </Tooltip>
+                                <Menu
+                                    sx={{ mt: '45px' }}
+                                    id="menu-appbar"
+                                    anchorEl={anchorElUser}
+                                    anchorOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    keepMounted
+                                    transformOrigin={{
+                                        vertical: 'top',
+                                        horizontal: 'right',
+                                    }}
+                                    open={Boolean(anchorElUser)}
+                                    onClose={handleCloseUserMenu}
+                                >
+                                    <MenuItem key='profile' onClick={handleCloseUserMenu}>
+                                        <Typography textAlign="center"> Profile </Typography>
+                                    </MenuItem>
+                                </Menu>
+                            </Box>
+                        </Toolbar>
+                    </Container>
+                </AppBar>
+            </ThemeProvider>
         </React.Fragment>
     )
 }
